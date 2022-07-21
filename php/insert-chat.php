53677c02086a3a2efffc9a70e1e4ecffb3e9d9b6
AndyfMaxwell <84532942+AndyfMaxwell@users.noreply.github.com>
2022-07-21 10:43:39 -0400

    updated with prepared statements

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -6,8 +6,10 @@
         $courseid = mysqli_real_escape_string($conn, $_POST['courseid']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
         if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO messages (courseid, outgoing_msgid, msg)
-                                        VALUES ('{$courseid}', '{$outgoing_msgid}', '{$message}')");
+            $sql = $conn->prepare("INSERT INTO messages (courseid, outgoing_msgid, msg)
+            VALUES (?, ?, ?)");
+            $sql->bind_param("sss", $courseid, $outgoing_msgid, $message);
+            $sql->execute();
         }
     }else{
         header("location: ../login.php");
--- php/signup.php
+++ php/signup.php
@@ -13,8 +13,16 @@
             }else{
                 $ran_id = rand(time(), 100000000);
                 $encrypt_pass = md5($password);
-                $insert_query = mysqli_query($conn, "INSERT INTO users (fname, lname, email, password, prof, uniqueid)
-                VALUE ('{$fname}', '{$lname}', '{$email}', '{$encrypt_pass}', '0', {$ran_id})");
+
+                $insert_query = $conn->prepare("INSERT INTO users (fname, lname, email, password, prof, uniqueid)
+                VALUE (?, ?, ?, ?, ?, ?)");
+
+                $prof = "0";
+
+                $insert_query->bind_param("ssssss", $fname, $lname, $email, $encrypt_pass, $prof, $ran_id);
+
+                $insert_query->execute();
+
                 if ($insert_query) {
                     $select_sql2 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
                     if(mysqli_num_rows($select_sql2) > 0) {
